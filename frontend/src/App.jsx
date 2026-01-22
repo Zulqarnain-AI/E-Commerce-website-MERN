19e582dd65a6c6264bc43d7c31f0b432ff82191f
Zulqarnain-AIgit config --global user.name Zulqarnain-AIgit config --global user.name Zulqarnain-AI <qarnainhassan786@gmail.com>
2026-01-22 22:29:49 +0500

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import LandingPage from './components/landingPage/LandingPage'
 
 function App() {
 
   return (
     <>
-      
+      <LandingPage />
     </>
   )
 }
